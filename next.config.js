86de22b64e20f4f97b89efa26e98a1abc6133715
KanizNUBTK <kaniz.m.mim@gmail.com>
2022-10-03 17:52:39 +0600

    error solved

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   reactStrictMode: false,
   trailingSlash: true,
-  // env: {
-  //   imgbb_key: '38881aa60af0a8c5bb11a08e11544d00',
-  //   BACKEND_URL: process.env.NODE_ENV === 'development' ?
-  //     'http://localhost:5000/' :
-  //     'https://backend.taxants.tk/',
-  //   user_login_mode:
-  //     process.env.NODE_ENV === "development"
-  //       ? "/login"
-  //       : "https://taxants.tk/login/",
-  // }
+  env: {
+
+  }
 };
